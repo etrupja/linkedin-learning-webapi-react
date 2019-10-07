ca705117e8da231bd7d7c363a42bb0d6636d6636
Ervis Trupja <ervistrupja@outlook.com>
2019-10-07 11:17:17 -0700

    Update Data.cs

--- Data/Data.cs
+++ Data/Data.cs
@@ -9,64 +9,48 @@ public static class Data
         new Trip()
         {
             Id=1,
-            Name="Visit 1",
-            Description="Visit 1 description...",
+            Name="Vienna, Austria",
+            Description="Vienna, is one the most beautiful cities in Austria and in Europe as well. Other than the Operas for which it is well known, Vienna also has many beautiful parks...",
             DateStarted = new DateTime(2017,01,20),
             DateCompleted = null
         },
         new Trip()
         {
             Id=2,
-            Name="Visit 2",
-            Description="Visit 2 description...",
+            Name="Carpinteria, CA, USA",
+            Description="Carpinteria is a city located on the central coast of California, just south of Santa Barbara. It has many beautiful beaches as well as a popular Avocado Festival which takes place every year in October...",
             DateStarted = new DateTime(2019,02,22),
             DateCompleted = new DateTime(2019,01,30)
         },
         new Trip()
         {
             Id=3,
-            Name="Visit 3",
-            Description="Visit 3 description...",
+            Name="San Francisco, CA, USA",
+            Description="San Francisco is a metropolitan area located on the west coast of the United States. Some popular tourist features include the Golden Gate Bridge, Chinatown, and Fisherman's Wharf. There are a variety of popular food options...",
             DateStarted = new DateTime(2019,01,27),
             DateCompleted = new DateTime(2019,01,30)
         },
         new Trip()
         {
             Id=4,
-            Name="Visit 4",
-            Description="Visit 4 description...",
+            Name="Tucson, AZ, USA",
+            Description="Tucson is a southwestern city in Arizona that is home to the University of Arizona. It was recently named a world gastronomy city, and is well known for its desert landscape and various bike races...",
             DateStarted = new DateTime(2019,01,20),
             DateCompleted = null
         },
         new Trip()
         {
             Id=5,
-            Name="Visit 5",
-            Description="Visit 6 description...",
+            Name="Albuquerque, NM, USA",
+            Description="Albuquerque is a city located in New Mexico that is famous for its balloon festivals, picturesque views and references to TV shows.",
             DateStarted = new DateTime(2015,01,20),
             DateCompleted = new DateTime(2015,01,30)
         },
         new Trip()
         {
             Id=7,
-            Name="Visit 7",
-            Description="Visit 7 description...",
-            DateStarted = new DateTime(2019,01,20),
-            DateCompleted = null
-        },
-        new Trip()
-        {
-            Id=8,
-            Name="Visit 8",
-            Description="Visit 8 description...",
-            DateStarted = new DateTime(2019,01,20),
-            DateCompleted = null
-        },
-        new Trip()
-        {
-            Id=9,
-            Name="Visit 9",
-            Description="Visit 9 description...",
+            Name="Munich, Germany",
+            Description="Munich is an important city in Germany, located in the heart of Bavaria. It's famous for its traditional Oktoberfest annual festival, and many nice lakes and parks...",
             DateStarted = new DateTime(2019,01,20),
             DateCompleted = null
         }
